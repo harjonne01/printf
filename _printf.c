1c13e36cfcb5f1172523ca7b5507d07c8dc8fd5f
John Familoye <fajob20@yahoo.com>
2023-10-17 05:03:40 -0700

    0x11. C - printf

--- _printf.c
+++ _printf.c
@@ -16,29 +16,53 @@ int _printf(const char *format, ...)
 
 	if (format == Null)
 		return (-1);
+
 	while (*format != '\0')
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
+			putchar(*format);
 			num_char++;
 		}
 		else
 		{
-			format++;
-			break;
-		}
-		if (*format == '%')
-			write(1, format, 1);
-		num_char++;
-		else if (*format == 'c')
-		{
-			char c = va_arg(args, int);
-
-			write(1, &c, 1);
+			putchar('%');
 			num_char++;
 		}
 	}
 	va_end(args);
 	return (num_char);
+}
+
+/**
+* print_char - Prints a character.
+* @args: argument list containing
+* the char to be printed.
+* Return: count of characters printed.
+*/
+
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	_putchar(c);
+	return (1);
+}
+
+/**
+* print_string - Prints a string.
+* @args: argument list containing
+* the string to be printed.
+* Return: length of string.
+*/
+
+int print_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+
+	for (s_len = 0; s[s_len] != '\0'; s_len++)
+	{
+		_putchar(s[s_len]);
+	}
+	return (s_len);
 }
