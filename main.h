85bbe77ee4c293cc04540fa5cda41b3484da858f
John Familoye <fajob20@yahoo.com>
2023-10-17 06:13:40 -0700

    0x11. C - printf

--- main.h
+++ main.h
@@ -1,14 +1,46 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/* Headers */
 #include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
 
-int _printf(const char *format, ...);
+/**
+ * struct printer - struct printer
+ * @printer: The printer
+ * @f: function pointer
+ */
+typedef struct printer
+{
+char printer;
+int (*f)(va_list);
+} printer_t;
+
+/* Constants */
+#define BUFSIZE 1024
+
+/* Prototypes */
 int _putchar(char c);
-int printf_integer(va_list args, int printed);
-int print_string(va_list args);
+int _printf(const char *format, ...);
 int print_char(va_list args);
-
-
-#endif
+int print_string(va_list args);
+int print_number(va_list args);
+int print_binary(va_list args);
+int print_reverse(va_list args);
+int (*get_print_func(const char s))(va_list);
+int _strlen(char *s);
+int print_percent(va_list args);
+int print_else(va_list args);
+int rot13(va_list args);
+char *_strchr(char *s, char c);
+int _puts(const char *str);
+int _put_unsigned(va_list args);
+int put_unsigned_hex(va_list args);
+int put_unsigned_hex_upper(va_list args);
+int _put_octal(va_list args);
+char *itoa(long int, int);
+int put_pointer(va_list args);
+int print_string_with_non_printable(va_list args);
+int print_non_printable_char(char c);
+#endif /* MAIN_H */
